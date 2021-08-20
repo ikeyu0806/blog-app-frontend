5dd95e32af58296d6ac318dce6916549fef07bae
ikeyu0806 <ikeyu0806@gmail.com>
2021-08-20 14:13:16 +0900

    create post page layout

--- pages/new_post.vue
+++ pages/new_post.vue
@@ -1,3 +1,30 @@
 <template>
-  <h1>記事投稿</h1>
+  <div>
+    <h2>記事を投稿する</h2>
+    <v-container fluid>
+      <v-form
+        ref="form"
+        v-model="valid"
+        lazy-validation
+      >
+        <v-text-field
+          label="タイトル"
+          outlined
+        ></v-text-field>
+        <v-textarea
+          label="本文"
+          outlined
+          name="post-content"
+          value=""
+          rows=30
+        ></v-textarea>
+        <v-btn
+          color="success"
+          class="mr-4"
+        >
+        送信
+      </v-btn>
+      </v-form>
+    </v-container>
+  </div>
 </template>
