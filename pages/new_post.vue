fce37a39b0fcaaee517cc5d338c53be2860b3b0a
ikeyu0806 <ikeyu0806@gmail.com>
2021-08-21 17:51:39 +0900

    create NewPost component

--- pages/new_post.vue
+++ pages/new_post.vue
@@ -1,30 +1,12 @@
 <template>
-  <div>
-    <h2>記事を投稿する</h2>
-    <v-container fluid>
-      <v-form
-        ref="form"
-        v-model="valid"
-        lazy-validation
-      >
-        <v-text-field
-          label="タイトル"
-          outlined
-        ></v-text-field>
-        <v-textarea
-          label="本文"
-          outlined
-          name="post-content"
-          value=""
-          rows=30
-        ></v-textarea>
-        <v-btn
-          color="success"
-          class="mr-4"
-        >
-        送信
-      </v-btn>
-      </v-form>
-    </v-container>
-  </div>
+  <NewPost></NewPost>
 </template>
+
+<script>
+import NewPost from '~/components/NewPost'
+export default {
+  components: {
+    NewPost
+  }
+}
+</script>
