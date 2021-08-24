a8dd20087b7eea8a144f636047a5d1876acad77a
ikeyu0806 <ikeyu0806@gmail.com>
2021-08-24 20:40:34 +0900

    add post index axios

--- components/Post/IndexPost.vue
+++ components/Post/IndexPost.vue
@@ -1,6 +1,35 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-    </v-col>
-  </v-row>
+  <v-simple-table>
+  <thead>
+    <tr>
+      <th class="text-left">
+        記事タイトル
+      </th>
+      <th class="text-left">
+        投稿ユーザ
+      </th>
+    </tr>
+  </thead>
+  <!-- <tbody>
+    <tr
+      v-for="post in posts"
+      :key="post.title"
+    >
+      <td>{{ post.title }}</td>
+      <td>投稿者</td>
+    </tr>
+  </tbody> -->
+  </v-simple-table>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  mounted: function () {
+    axios.get('http://localhost:5001/posts')
+      .then(response => console.log(response))
+      .catch(error => console.log(error))
+  }
+}
+</script>
